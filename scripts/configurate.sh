d5cae6cd816417e2e3334cd28e945b3290521c95
CrazyOverdose <alenkavh@yandex.ru>
2019-10-05 19:59:12 +0300

    test12

--- scripts/configurate.sh
+++ scripts/configurate.sh
@@ -4,4 +4,3 @@ set -ex
 
 cmake -H. -B .build || 1
 ./scripts/hunter-gtest-fix.sh
-
--- scripts/tests.sh
+++ scripts/tests.sh
@@ -1,12 +1,5 @@
 #! /bin/sh
 
-set -ex
+./scripts/tests.sh
 
-./scripts/configurate.sh
-
-export GTEST_COLOR=1
-CMAKE_CONFIG_OPTS="-DCMAKE_BUILD_TYPE=Debug"
-CMAKE_OPTS="$CMAKE_CONFIG_OPTS"
-cmake -H. -B .build $CMAKE_OPTS
-cmake --build .build
-cmake --build .build --target test -- ARGS="--verbose"
+cd .build && valgrind ./tests
