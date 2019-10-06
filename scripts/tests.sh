e53db37546dc14c0eec44ef68538a20f49086c0b
CrazyOverdose <alenkavh@yandex.ru>
2019-10-06 17:37:21 +0300

    test20

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -1,5 +1,12 @@
 #! /bin/sh
 
-./scripts/tests.sh
+set -ex
 
-cd .build && valgrind ./tests
+./scripts/configurate.sh
+
+export GTEST_COLOR=1
+CMAKE_CONFIG_OPTS="-DCMAKE_BUILD_TYPE=Debug"
+CMAKE_OPTS="$CMAKE_CONFIG_OPTS"
+cmake -H. -B .build $CMAKE_OPTS
+cmake --build .build
+cmake --build .build --target test -- ARGS="--verbose"
