44b24dc46bd8ee3b4cf3812e9e6d3ee7e49a18b1
CrazyOverdose <alenkavh@yandex.ru>
2019-10-06 18:57:06 +0300

    test27

--- .build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/absinthetoxin/lab-01-parser
-# Build directory: /home/absinthetoxin/lab-01-parser/.build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(unit_tests "/home/absinthetoxin/lab-01-parser/.build/tests")
-set_tests_properties(unit_tests PROPERTIES  _BACKTRACE_TRIPLES "/home/absinthetoxin/lab-01-parser/CMakeLists.txt;51;add_test;/home/absinthetoxin/lab-01-parser/CMakeLists.txt;0;")
